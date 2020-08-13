7d64c9bd15c37d930a2c38aaafd726a6a39617b7
Ahmet Kandemir PEHLİVANLI <ahmet.pehlivanli@yahoo.com>
2020-08-13 21:43:29 +0300

    New features have been added.
    New functions, such as setter-member functions and the ones used to Save to Text File, have been added.

--- Vectors_I.h
+++ Vectors_I.h
@@ -1,6 +1,6 @@
 #ifndef LEARNINGCPP_H
 #define LEARNINGCPP_H
-//#define DebugMessages // Used to activa debugging messages. ( Make it no comment )
+//#define DebugMessages // Used to activa debugging messages. ( Make it uncomment )
 
 /******************************* A Place To Define Related Macros ******************************/
 #ifdef  DebugMessages
@@ -13,25 +13,43 @@
 
 /******************************* A Place To Define Related Libraries ***************************/
 #include <iostream>
+#include <fstream>
 #include <time.h>
+#include <ctype.h>
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <iomanip>
 
 /******************************* A Place To Define Global Variables *****************************/
 class NotLearning {
 public:
     NotLearning(); // Constructor
+
     int getID()const; // Function to used to initialise private variable "ID" of the class via the constructor.
+    void setID(int);
+    
     int getDay()const; // Function to used to initialise private variable "day" of the class via the constructor.
+    void setDay(int);
+    
     int getYear()const; // Function to used to initialise private variable "year" of the class via the constructor.
+    void setYear(int);
     
     double getAverage()const; // Function to used to initialise private variable "average" of the class via the constructor.
+    void setAverage(double);
 
     std::string getName()const; // Function to used to initialise private variable "name" of the class via the constructor.
+    void setName(std::string);
+    
     std::string getSurname()const; // Function to used to initialise private variable "surname" of the class via the constructor.
+    void setSurname(std::string);
+    
     std::string getCity()const; // Function to used to initialise private variable "city" of the class via the constructor.
+    void setCity(std::string);
+    
     std::string getMonth()const; // Function to used to initialise private variable "month" of the class via the constructor.
+    std::string setMonth(std::string);
+
 
 private:
     int ID;
@@ -47,8 +65,10 @@ private:
 };
 
 /******************************* A Place To Define Global Functions *******************************/
-void fillVector(std::vector<NotLearning>&);
-void showVector(const std::vector<NotLearning>);
+void fillVector(std::vector<NotLearning>&, const int);
+void showVector(const std::vector<NotLearning>&);
+void saveVector(const std::vector<NotLearning>&);
+void copyVector(const std::vector<NotLearning>&, std::vector<NotLearning>&);
 void sortVectorByID(std::vector<NotLearning>&);
 void sortVectorByAverage(std::vector<NotLearning>&);
 void sortVectorByName(std::vector<NotLearning>&);
