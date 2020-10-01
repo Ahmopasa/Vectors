0f7eb75afe0b821610d9c8a1057652695fcf34ad
Ahmet Kandemir PEHLİVANLI <ahmet.pehlivanli@yahoo.com>
2020-10-01 22:01:11 +0300

    Class name changed.
    The new class name is "MyVector" and some typo fixed.

--- LearningVectors.h
+++ LearningVectors.h
@@ -5,8 +5,8 @@
 /******************************* A Place To Define Related Macros ******************************/
 #ifdef  DebugMessages
 #define DebugMessage_CallingConstructor std::cout << "Constructor was Called!" << std::endl
-#define DebugMessage_CallingDeconstructor std::cout << "Deconstructor was Called!" << std::endl
-#else  
+#define DebugMessage_CallingDeconstructor std::cout << "Deconstruction was Called!" << std::endl
+#else
 #define DebugMessage_CallingConstructor
 #define DebugMessage_CallingDeconstructor
 #endif
@@ -22,59 +22,59 @@
 #include <iomanip>
 
 /******************************* A Place To Define Global Variables *****************************/
-class NotLearning {
+class MyVector {
 public:
-    NotLearning(); // Constructor
+	MyVector(); // Constructor
 
-    int getID()const; // Function to used to initialise private variable "ID" of the class via the constructor.
-    void setID(int);
-    
-    int getDay()const; // Function to used to initialise private variable "day" of the class via the constructor.
-    int getYear()const; // Function to used to initialise private variable "year" of the class via the constructor.
-   
-    double getAverage()const; // Function to used to initialise private variable "average" of the class via the constructor.
-    void setAverage(double);
+	int getID()const; // Function to used to initialize private variable "ID" of the class via the constructor.
+	void setID(int);
 
-    std::string getName()const; // Function to used to initialise private variable "name" of the class via the constructor.
-    void setName(char*);
-    
-    std::string getSurname()const; // Function to used to initialise private variable "surname" of the class via the constructor.
-    void setSurname(char* );
-    
-    std::string getCity()const; // Function to used to initialise private variable "city" of the class via the constructor.
-    void setCity(char* );
-    
-    std::string getMonth()const; // Function to used to initialise private variable "month" of the class via the constructor.
+	int getDay()const; // Function to used to initialize private variable "day" of the class via the constructor.
+	int getYear()const; // Function to used to initialize private variable "year" of the class via the constructor.
 
-    void setDates(int, char*, int);
-    
+	double getAverage()const; // Function to used to initialize private variable "average" of the class via the constructor.
+	void setAverage(double);
+
+	std::string getName()const; // Function to used to initialize private variable "name" of the class via the constructor.
+	void setName(char*);
+
+	std::string getSurname()const; // Function to used to initialize private variable "surname" of the class via the constructor.
+	void setSurname(char*);
+
+	std::string getCity()const; // Function to used to initialize private variable "city" of the class via the constructor.
+	void setCity(char*);
+
+	std::string getMonth()const; // Function to used to initialize private variable "month" of the class via the constructor.
+
+	void setDates(int, const char*, int);
+
 private:
-    int ID;
-    int day;
-    int year;
-    
-    double average;
+	int ID;
+	int day;
+	int year;
 
-    std::string name;
-    std::string surname;
-    std::string city;
-    std::string month;
+	double average;
+
+	std::string name;
+	std::string surname;
+	std::string city;
+	std::string month;
 };
 
 /******************************* A Place To Define Global Functions *******************************/
-void fillVector(std::vector<NotLearning>&, const int);
-void fillVectorByText(std::vector<NotLearning>&, const int);
-void showVector(const std::vector<NotLearning>&);
-void saveVector(const std::vector<NotLearning>&);
-void copyVector(const std::vector<NotLearning>&, std::vector<NotLearning>&);
-void sortVectorByID(std::vector<NotLearning>&);
-void sortVectorByAverage(std::vector<NotLearning>&);
-void sortVectorByName(std::vector<NotLearning>&);
-void sortVectorBySurname(std::vector<NotLearning>&);
-void sortVectorByCity(std::vector<NotLearning>&);
-void sortVectorByDay(std::vector<NotLearning>&);
-void sortVectorByMonth(std::vector<NotLearning>&);
-void sortVectorByYear(std::vector<NotLearning>&);
-void sortVectorByDetail(std::vector<NotLearning>&);
+void fillVector(std::vector<MyVector>&, const int);
+void fillVectorByText(std::vector<MyVector>&, const int);
+void showVector(const std::vector<MyVector>&);
+void saveVector(const std::vector<MyVector>&);
+void copyVector(const std::vector<MyVector>&, std::vector<MyVector>&);
+void sortVectorByID(std::vector<MyVector>&);
+void sortVectorByAverage(std::vector<MyVector>&);
+void sortVectorByName(std::vector<MyVector>&);
+void sortVectorBySurname(std::vector<MyVector>&);
+void sortVectorByCity(std::vector<MyVector>&);
+void sortVectorByDay(std::vector<MyVector>&);
+void sortVectorByMonth(std::vector<MyVector>&);
+void sortVectorByYear(std::vector<MyVector>&);
+void sortVectorByDetail(std::vector<MyVector>&);
 
 #endif // !LEARNINGVECTORS_H
