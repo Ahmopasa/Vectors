1ff43e2c9c36954eace59c3a5970fa3519079454
Ahmet Kandemir PEHLİVANLI <ahmet.pehlivanli@yahoo.com>
2020-10-02 07:08:46 +0300

    More Cpp Features Have Been Added
    I'm one step closer to fill the vector from text files. To achieve that new functions have been added. Some already written functions were revamped.

--- LearningVectors.cpp
+++ LearningVectors.cpp
@@ -1,46 +1,62 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include "LearningVectors.h"
 
-#define check_leap(year) ((year) % 4 == 0 && ((year) % 100 != 0 || (year) % 400 == 0)) // Return 1 / 0 if the year is leap year or not.
+/******************************* A Place To Define Related Macros ******************************/
+#define check_leap(year) ((year) % 4 == 0 && ((year) % 100 != 0 || (year) % 400 == 0))
 
+/******************************* A Place To Define Global Variables *****************************/
+
+/******************************* A Place To Define Class Members *****************************/
 MyVector::MyVector()
 {
 	ID = rand() % 1001 + 101;
 	average = ((double)rand() * (100. - 0.)) / (double)RAND_MAX + 0.;
 
 	static const std::string nameList[] = { "AHMET", "ABRAHAM", "ABIGAIL", "ABBIE", "AHMET", "ELIZABETH", "ELLA", "ELIZABETH", "AHMET", "ELLEN", "ELLEN", "ELLEN", "EPIPHANY", "EPPIE", "ETTA", "EULA", "FOREST", "FORD", "FOSTER", "FOX",
-									"FOREST", "FORD", "FOSTER", "FOX", "EPIPHANY", "EPPIE", "ETTA", "EULA", "ABIGAIL", "ABBIE", "AHMET", "ELIZABETH", "ELLA", "ABRAHAM", "ABIGAIL", "ABBIE", "AHMET", "ELIZABETH", "ELLA", "EULA",
-									"AHMET", "ABRAHAM", "ABIGAIL", "ABBIE", "AHMET", "ELIZABETH", "ELLA", "ELIZABETH", "AHMET", "ELLEN", "ELLEN", "ELLEN", "EPIPHANY", "EPPIE", "ETTA", "EULA", "FOREST", "FORD", "FOSTER", "FOX",
-									"FOREST", "FORD", "FOSTER", "FOX", "EPIPHANY", "EPPIE", "ETTA", "EULA", "ABIGAIL", "ABBIE", "AHMET", "ELIZABETH", "ELLA", "ABRAHAM", "ABIGAIL", "ABBIE", "AHMET", "ELIZABETH", "ELLA", "EULA",
-	};
-	name = nameList[rand() % 80];
+								"FOREST", "FORD", "FOSTER", "FOX", "EPIPHANY", "EPPIE", "ETTA", "EULA", "ABIGAIL", "ABBIE", "AHMET", "ELIZABETH", "ELLA", "ABRAHAM", "ABIGAIL", "ABBIE", "AHMET", "ELIZABETH", "ELLA", "EULA",
+								"AHMET", "ABRAHAM", "ABIGAIL", "ABBIE", "AHMET", "ELIZABETH", "ELLA", "ELIZABETH", "AHMET", "ELLEN", "ELLEN", "ELLEN", "EPIPHANY", "EPPIE", "ETTA", "EULA", "FOREST", "FORD", "FOSTER", "FOX",
+								"FOREST", "FORD", "FOSTER", "FOX", "EPIPHANY", "EPPIE", "ETTA", "EULA", "ABIGAIL", "ABBIE", "AHMET", "ELIZABETH", "ELLA", "ABRAHAM", "ABIGAIL", "ABBIE", "AHMET", "ELIZABETH", "ELLA", "EULA",
+	}; name = nameList[rand() % 80];
 
 	static const std::string surnameList[] = { "JOHNSON", "ABBESTEGE", "WILSON", "JOHNSON", "ABBESTEGE", "HARRIS", "JOHNSON", "HARRIS", "WILSON", "JOHNSON", "GARCIA", "GARCIA", "JOHNSON", "RODRIGUEZ", "RODRIGUEZ", "COLEMAN", "WILSON",
-									  "JOHNSON", "ABBESTEGE", "WILSON", "JOHNSON",	"GARCIA", "GARCIA", "JOHNSON", "RODRIGUEZ", "RODRIGUEZ"	,  "ABBESTEGE", "HARRIS", "JOHNSON", "HARRIS", "WILSON", "JOHNSON", "GARCIA",
-									  "JOHNSON", "ABBESTEGE", "WILSON", "JOHNSON", "ABBESTEGE", "HARRIS", "JOHNSON", "HARRIS", "WILSON", "JOHNSON", "GARCIA", "GARCIA", "JOHNSON", "RODRIGUEZ", "RODRIGUEZ", "COLEMAN", "WILSON",
-									  "WILSON", "JOHNSON", "GARCIA", "GARCIA", "JOHNSON", "RODRIGUEZ", "RODRIGUEZ", "COLEMAN", "WILSON", "JOHNSON", "ABBESTEGE", "WILSON", "ABBESTEGE", "HARRIS", "JOHNSON", "HARRIS",
-									  "WILSON", "JOHNSON", "GARCIA", "GARCIA", "JOHNSON", "RODRIGUEZ", "RODRIGUEZ", "COLEMAN", "WILSON", "JOHNSON", "ABBESTEGE", "WILSON", "JOHNSON", "ABBESTEGE",
-	};
-	surname = surnameList[rand() % 80];
+								  "JOHNSON", "ABBESTEGE", "WILSON", "JOHNSON",	"GARCIA", "GARCIA", "JOHNSON", "RODRIGUEZ", "RODRIGUEZ"	,  "ABBESTEGE", "HARRIS", "JOHNSON", "HARRIS", "WILSON", "JOHNSON", "GARCIA",
+								  "JOHNSON", "ABBESTEGE", "WILSON", "JOHNSON", "ABBESTEGE", "HARRIS", "JOHNSON", "HARRIS", "WILSON", "JOHNSON", "GARCIA", "GARCIA", "JOHNSON", "RODRIGUEZ", "RODRIGUEZ", "COLEMAN", "WILSON",
+								  "WILSON", "JOHNSON", "GARCIA", "GARCIA", "JOHNSON", "RODRIGUEZ", "RODRIGUEZ", "COLEMAN", "WILSON", "JOHNSON", "ABBESTEGE", "WILSON", "ABBESTEGE", "HARRIS", "JOHNSON", "HARRIS",
+								  "WILSON", "JOHNSON", "GARCIA", "GARCIA", "JOHNSON", "RODRIGUEZ", "RODRIGUEZ", "COLEMAN", "WILSON", "JOHNSON", "ABBESTEGE", "WILSON", "JOHNSON", "ABBESTEGE",
+	}; surname = surnameList[rand() % 80];
 
 	static const std::string  cityList[] = { "Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Colorado",
-								 "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Connecticut", "Delaware", "Louisiana", "Maine", "Maryland", "Massachusetts",
-								 "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Connecticut", "Delaware",
-								 "Delaware", "Florida", "Georgia", "Hawaii", "Connecticut", "Delaware", "Louisiana", "Maine", "Maryland", "Massachusetts", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Connecticut", "Delaware",
-								 "Florida", "Georgia", "Hawaii", "Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida"
-	};
-	city = cityList[rand() % 80];
+							 "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Connecticut", "Delaware", "Louisiana", "Maine", "Maryland", "Massachusetts",
+							 "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Connecticut", "Delaware",
+							 "Delaware", "Florida", "Georgia", "Hawaii", "Connecticut", "Delaware", "Louisiana", "Maine", "Maryland", "Massachusetts", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Connecticut", "Delaware",
+							 "Florida", "Georgia", "Hawaii", "Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida"
+	}; city = cityList[rand() % 80];
 
-	year = rand() % (2020 - 1989 + 1) + 1989;
-	static const int daytabs[][13] = {
-				{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
-				{0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
-	};
+	int random_month = rand() % 12 + 1;
 	static const std::string pmons[] = { "","January","February","March","April","May","June","July","August","September","October","November","December" };
-	int counter_date = rand() % 12 + 1;
-	int tempDay = daytabs[check_leap(year)][counter_date];
+	month = pmons[random_month];
+
+	static const int daytabs[][13] = {
+			{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
+			{0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
+	}; 
+	year = rand() % (2020 - 1989 + 1) + 1989;
+	int tempDay = daytabs[check_leap(year)][random_month];
 	day = rand() % tempDay + 1;
-	month = pmons[counter_date];
+}
+
+MyVector::MyVector(std::string& ID, std::string& average, std::string& name, std::string& surname, std::string& city, std::string& day, std::string& month, std::string& year)
+{
+	//std::cout << "Before Single Push Back => " << average << " " << name << " " << surname << " " << city << " " << day << " " << month << " " << year << "\n";
+
+	setID(ID);
+	setAverage(average);
+	setName(name);
+	setSurname(surname);
+	setCity(city);
+    setDates(day, month, year);
+
+	//std::cout << "Before Single Push Back 2  => " << getID() << " " << getAverage() << " " << getName() << " " << getSurname() << " " << getCity() << " " <<getDay() << " " << getMonth() << " " << getYear() << "\n";
 }
 
 int MyVector::getID()const
@@ -48,9 +64,9 @@ int MyVector::getID()const
 	return ID;
 }
 
-void MyVector::setID(int newID)
+void MyVector::setID(std::string& newID)
 {
-	ID = newID;
+	ID = std::stoi(newID);	 
 }
 
 double MyVector::getAverage()const
@@ -58,9 +74,9 @@ double MyVector::getAverage()const
 	return average;
 }
 
-void MyVector::setAverage(double newAverage)
+void MyVector::setAverage(std::string& temp_average)
 {
-	this->average = newAverage;
+	average = std::stod(temp_average);
 }
 
 std::string MyVector::getName()const
@@ -68,7 +84,7 @@ std::string MyVector::getName()const
 	return name;
 }
 
-void MyVector::setName(std::string _newName)
+void MyVector::setName(std::string& _newName)
 {
 	this->name = _newName;
 }
@@ -78,7 +94,7 @@ std::string MyVector::getSurname()const
 	return surname;
 }
 
-void MyVector::setSurname(std::string _newSurname)
+void MyVector::setSurname(std::string& _newSurname)
 {
 	this->surname = _newSurname;
 }
@@ -88,7 +104,7 @@ std::string MyVector::getCity()const
 	return city;
 }
 
-void MyVector::setCity(std::string _newCity)
+void MyVector::setCity(std::string& _newCity)
 {
 	this->city = _newCity;
 }
@@ -98,44 +114,38 @@ int MyVector::getDay()const
 	return day;
 }
 
-int MyVector::getYear()const
-{
-	return year;
-}
-
 std::string MyVector::getMonth()const
 {
 	return month;
 }
 
-void MyVector::setDates(int newYear, const std::string newMonth, int newDay)
+int MyVector::getYear()const
 {
-	year = newYear;
-
-	static const int daytabs[][13] = {
-				{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
-				{0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
-	};
-	static const std::string pmons[] = { "","January","February","March","April","May","June","July","August","September","October","November","December" };
-
-	int month_counter = 0;
-	for (int i = 0; i < 13; i++)
-	{
-		if (!((this->month).compare(newMonth)))
-			month_counter++;
-	}
-	int tempDay = daytabs[check_leap(year)][month_counter];
-	if (newDay <= tempDay)
-	{
-		day = newDay;
-	}
-
-	this->month = newMonth;
+	return year;
 }
 
-void fillVector(std::vector<MyVector>& newClass, const int& TotalStudent)
+void MyVector::setDates (std::string& newDay, std::string& newMonth, std::string& newYear)
 {
-	//srand((unsigned int)time(NULL));
+
+	static const std::string pmons[] = { "","January","February","March","April","May","June","July","August","September","October","November","December" };
+	int i;
+	for (i = 0; i < 13; i++)
+		if (!pmons[i].compare(newMonth))
+			month = newMonth;
+
+	static const int daytabs[][13] = {
+			{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
+			{0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
+	};
+	year = std::stoi(newYear);
+	int tempDay = std::stoi(newDay);
+	int indexDay = daytabs[check_leap(year)][i];
+	if (tempDay <= indexDay)
+		day = tempDay;
+}
+
+void fillVectorRandomly(std::vector<MyVector>& newClass, const int& TotalStudent)
+{
 	if (TotalStudent > 0)
 	{
 		for (int i = 0; i < TotalStudent; i++)
@@ -151,6 +161,56 @@ void fillVector(std::vector<MyVector>& newClass, const int& TotalStudent)
 	}
 }
 
+void fillVectorViaText(std::vector<MyVector>& newClass)
+{
+	std::ifstream FileHandler;
+	std::vector<std::string> temp_studentID;
+	std::string temp_string;
+
+	FileHandler.open("List_Of_Vector_Elements");
+	if (FileHandler.good())
+	{
+		while (FileHandler.good())
+		{
+			std::getline(FileHandler, temp_string);
+			temp_studentID.push_back(temp_string);
+		}
+		FileHandler.close();
+	}
+	else
+	{
+		std::cout << "Unable to open file: List_Of_Vector_Elements. " << "\n";
+	}
+
+	const unsigned int Class_Vector_Size = temp_studentID.size();
+	
+	std::string ID = "temp_ID";
+	std::string average = "temp_average";
+	std::string surname = "temp_surname";
+	std::string city = "temp_city";
+	std::string name = "temp_name";
+	std::string day = "temp_day";
+	std::string month = "temp_month";
+	std::string year = "temp_year";
+
+	for (unsigned int i = 0; i < Class_Vector_Size; i++)
+	{
+		std::ifstream MainHandler;
+		MainHandler.open(temp_studentID[i]);
+		while (MainHandler >> average >> name >> surname >> city >> day >> month >> year)
+		{
+			std::cout << "Before New Vector       => "<< average << " " << name << " " << surname << " " << city << " " << day << " " << month << " " << year << "\n";
+			MyVector newstudent(temp_studentID[i], average, name, surname, city, day, month, year);
+			newClass.push_back(newstudent);
+			
+			std::cout << "After Single Push Back  => " << newClass[i].getID() << " " << newClass[i].getAverage() << " " << newClass[i].getName() << " " << newClass[i].getSurname() << " " << newClass[i].getCity() << " " << newClass[i].getDay() << " " << newClass[i].getMonth() << " " << newClass[i].getYear() << "\n";
+			
+			std::cout << "\n";
+		}
+		MainHandler.close();
+	}
+}
+
 void showVector(const std::vector<MyVector>& newClass)
 {
 	size_t size = newClass.size();
@@ -161,7 +221,7 @@ void showVector(const std::vector<MyVector>& newClass)
 	{
 		std::cout << "Person:" << std::left << std::setw(2) << i << " => " << std::flush;
 		std::cout << std::left << std::setw(4) << newClass[i].getID() << " " << std::flush;
-		std::cout << std::left << std::setw(7) << std::setprecision(2) << newClass[i].getAverage() << " " << std::flush;
+		std::cout << std::left << std::setw(7) << std::setprecision(4) << newClass[i].getAverage() << " " << std::flush;
 		std::cout << std::left << std::setw(10) << newClass[i].getName() << " " << std::flush;
 		std::cout << std::left << std::setw(10) << newClass[i].getSurname() << " " << std::flush;
 		std::cout << std::left << std::setw(15) << newClass[i].getCity() << " " << std::flush;
@@ -175,18 +235,27 @@ void showVector(const std::vector<MyVector>& newClass)
 
 void saveVector(const std::vector<MyVector>& newClass)
 {
-	std::vector<std::string> FileNames;
-	for (unsigned int i = 0; i < 5; i++)
+	const unsigned int Vector_Size = newClass.size();
+	std::ofstream* FileHandler = new std::ofstream[Vector_Size];
+	if (!FileHandler)
 	{
-		FileNames.push_back(std::to_string(newClass[i].getID()));
+		std::cout << "Memory allocation was failed to save vector in text file." << "\n";
 	}
-
-	for (unsigned int i = 0; i < FileNames.size(); i++)
+	else
 	{
-		std::ofstream FileHandler;
-		FileHandler.open(FileNames[i]);
-		FileHandler << newClass[i].getAverage() << " " << newClass[i].getName() << " " << newClass[i].getSurname() << " " << newClass[i].getCity() << " " << newClass[i].getDay() << " " << newClass[i].getMonth() << " " << " " << newClass[i].getYear() << " " << std::endl;
-		FileHandler.close();
+		for (unsigned int i = 0; i < Vector_Size; i++)
+		{
+			FileHandler[i].open(std::to_string(newClass[i].getID()));
+			FileHandler[i] << newClass[i].getAverage() << " " << newClass[i].getName() << " " << newClass[i].getSurname() << " " << newClass[i].getCity() << " " << newClass[i].getDay() << " " << newClass[i].getMonth() << " " << " " << newClass[i].getYear() << " " << std::endl;
+
+			FileHandler[i].close();
+
+			std::ofstream MainHandler;
+			MainHandler.open("List_Of_Vector_Elements", std::ios_base::app);
+			MainHandler << newClass[i].getID() << "\n";
+			MainHandler.close();
+		}
+		delete[] FileHandler;
 	}
 }
 
@@ -195,47 +264,47 @@ void copyVector(const std::vector<MyVector>& newClass, std::vector<MyVector>& my
 	myNewClass.assign(newClass.begin(), newClass.end());
 }
 
-bool cmp_by_id(const MyVector& one, const MyVector& two) // compare classes inside the vector by checking the member of "ID".
+static bool cmp_by_id(const MyVector& one, const MyVector& two) // compare classes inside the vector by checking the member of "ID".
 {
 	return one.getID() < two.getID();
 }
 
-bool cmp_by_average(const MyVector& one, const MyVector& two) // compare classes inside the vector by checking the member of "ID".
+static bool cmp_by_average(const MyVector& one, const MyVector& two) // compare classes inside the vector by checking the member of "ID".
 {
 	return (one.getAverage() < two.getAverage());
 }
 
-bool cmp_by_name(const MyVector& one, const MyVector& two) // compare classes inside the vector by checking the member of "ID".
+static bool cmp_by_name(const MyVector& one, const MyVector& two) // compare classes inside the vector by checking the member of "ID".
 {
 	return one.getName() < two.getName();
 }
 
-bool cmp_by_surname(const MyVector& one, const MyVector& two) // compare classes inside the vector by checking the member of "ID".
+static bool cmp_by_surname(const MyVector& one, const MyVector& two) // compare classes inside the vector by checking the member of "ID".
 {
 	return one.getSurname() < two.getSurname();
 }
 
-bool cmp_by_city(const MyVector& one, const MyVector& two) // compare classes inside the vector by checking the member of "ID".
+static bool cmp_by_city(const MyVector& one, const MyVector& two) // compare classes inside the vector by checking the member of "ID".
 {
 	return one.getCity() < two.getCity();
 }
 
-bool cmp_by_day(const MyVector& one, const MyVector& two) // compare classes inside the vector by checking the member of "ID".
+static bool cmp_by_day(const MyVector& one, const MyVector& two) // compare classes inside the vector by checking the member of "ID".
 {
 	return one.getDay() < two.getDay();
 }
 
-bool cmp_by_month(const MyVector& one, const MyVector& two) // compare classes inside the vector by checking the member of "month".
+static bool cmp_by_month(const MyVector& one, const MyVector& two) // compare classes inside the vector by checking the member of "month".
 {
 	return one.getMonth() < two.getMonth();
 }
 
-bool cmp_by_year(const MyVector& one, const MyVector& two) // compare classes inside the vector by checking the member of "year".
+static bool cmp_by_year(const MyVector& one, const MyVector& two) // compare classes inside the vector by checking the member of "year".
 {
 	return one.getYear() < two.getYear();
 }
 
-bool cmp_by_detail(const MyVector& one, const MyVector& two) // compare classes inside the vector by checking the member of "ID", then "average", etc...
+static bool cmp_by_detail(const MyVector& one, const MyVector& two) // compare classes inside the vector by checking the member of "ID", then "average", etc...
 {
 	if (one.getID() != two.getID())
 	{
