ec70205d8539d9eb119bccee5d2222e4af65dfd9
Ahmet Kandemir PEHLİVANLI <ahmet.pehlivanli@yahoo.com>
2021-01-21 19:34:08 +0300

    A couple of sentences were removed.
    In order to make the code look clearer, I have deleted a couple of line from main.cpp file.

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 
 int main()
 {
-<<<<<<< HEAD
 	std::vector<MyVector> my_student_list;
 
 	//fillVectorRandomly(my_student_list, 3);
@@ -13,7 +12,5 @@ int main()
 	showVector(my_student_list);
 	sortVectorByAverage(my_student_list);
 	showVector(my_student_list);
-=======
->>>>>>> a2f680a61918064b71aad918cde520d55730a218
 
 }
